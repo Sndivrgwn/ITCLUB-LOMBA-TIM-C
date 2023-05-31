a1441fe78d0d9f7b7bbbb3c762eede920c780c56
nurkalam abdurrahman <nurkalam.az@gmail.com>
2023-05-31 08:30:40 +0700

    perbaikan link

--- assets/js/script_page.js
+++ assets/js/script_page.js
@@ -17,6 +17,9 @@ let card_6 = document.getElementById("hilang_6");
 let card_7 = document.getElementById("hilang_7");
 let card_8 = document.getElementById("hilang_8");
 let card_9 = document.getElementById("hilang_9");
+let card_10 = document.getElementById("hilang_10");
+let card_11 = document.getElementById("hilang_11");
+let card_12 = document.getElementById("hilang_12");
 let button_left = document.getElementById("button_left_berita");
 let button_right = document.getElementById("button_right_berita");
 let button_nav_berita = document.getElementById("button_nav_berita_terkini");
@@ -135,6 +138,9 @@ paging_1.addEventListener("click", function () {
   card_7.style.display = "none";
   card_8.style.display = "none";
   card_9.style.display = "none";
+  card_10.style.display = "none";
+  card_11.style.display = "none";
+  card_12.style.display = "none";
 });
 
 paging_2.addEventListener("click", function () {
@@ -146,9 +152,12 @@ paging_2.addEventListener("click", function () {
   card_1.style.display = "none";
   card_2.style.display = "none";
   card_3.style.display = "none";
-  card_4.style.setProperty("display", "flex", "important");
-  card_5.style.setProperty("display", "flex", "important");
-  card_6.style.setProperty("display", "flex", "important");
+  card_4.style.display = "none";
+  card_5.style.display = "none";
+  card_6.style.display = "none";
+  card_10.style.setProperty("display", "flex", "important");
+  card_11.style.setProperty("display", "flex", "important");
+  card_12.style.setProperty("display", "flex", "important");
   card_7.style.setProperty("display", "flex", "important");
   card_8.style.setProperty("display", "flex", "important");
   card_9.style.setProperty("display", "flex", "important");
@@ -169,6 +178,9 @@ paging_3.addEventListener("click", function () {
   card_7.style.setProperty("display", "flex", "important");
   card_8.style.setProperty("display", "flex", "important");
   card_9.style.setProperty("display", "flex", "important");
+  card_10.style.display = "none";
+  card_11.style.display = "none";
+  card_12.style.display = "none";
 });
 
 paging_4.addEventListener("click", function () {
@@ -186,6 +198,9 @@ paging_4.addEventListener("click", function () {
   card_7.style.display = "none";
   card_8.style.display = "none";
   card_9.style.display = "none";
+  card_10.style.display = "none";
+  card_11.style.display = "none";
+  card_12.style.display = "none";
 });
 paging_5.addEventListener("click", function () {
   paging_5.style.backgroundColor = "#eaa51b";
@@ -202,4 +217,34 @@ paging_5.addEventListener("click", function () {
   card_7.style.display = "none";
   card_8.style.display = "none";
   card_9.style.display = "none";
+  card_10.style.display = "none";
+  card_11.style.display = "none";
+  card_12.style.display = "none";
 });
+
+function searchBerita() {
+  var input = document.querySelector('.input_berita_terkini');
+  var keyword = input.value.toLowerCase();
+
+  var cards = document.querySelectorAll('.card_berita_terkini .card');
+
+  var count = 0; 
+  cards.forEach(function(card) {
+    var title = card.querySelector('.card-tittle').textContent.toLowerCase();
+
+    if (title.includes(keyword)) {
+      if (count < 6) {
+        card.style.display = 'block';
+        count++;
+      } else {
+        card.style.display = 'none';
+      }
+    } else {
+      card.style.display = 'none';
+    }
+  });
+}
+
+var inputCari = document.querySelector('.input_berita_terkini');
+inputCari.addEventListener('input', searchBerita);
+
